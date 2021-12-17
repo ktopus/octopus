ac12820cc2fb3df6354901101110f39e09069b0c
Sergey N. Yatskevich <s.yatskevich@corp.mail.ru>
2021-12-17 12:30:44 +0300

    Try to fix compilation error

--- box.m
+++ box.m
@@ -797,7 +797,7 @@ build_secondary (struct object_space* _osp)
 	//
 	// Индексы типа хэш
 	//
-	Hash* hashes[MAX_IDX] = {nil,};
+	Hash<HashIndex>* hashes[MAX_IDX] = {nil,};
 	int hash_count = 0;
 
 	//
@@ -812,7 +812,7 @@ build_secondary (struct object_space* _osp)
 			if ([_osp->index[i] isKindOf:[Tree class]])
 				trees[tree_count++] = (Tree*)_osp->index[i];
 			else if ([_osp->index[i] isKindOf:[Hash class]])
-				hashes[hash_count++] = (Hash*)_osp->index[i];
+				hashes[hash_count++] = (Hash<HashIndex>*)_osp->index[i];
 			else
 				panic ("object_space = %" PRIu32 ", index = %" PRIu32 " is not a tree or hash", _osp->n, i);
 		}
